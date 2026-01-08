a7542b5d5468eee2f942e045f57b7efa574a9580
sakika774 <sakikalab74@gmail.com>
2026-01-08 15:40:33 +0900

    feat: add Map component with sample marker

---
+++ src/components/Map.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+
+const Map = () => {
+  const position: [number, number] = [35.6895, 139.6917]; // 東京の緯度経度
+
+  return (
+    <MapContainer center={position} zoom={13} style={{ height: '500px', width: '100%' }}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a>'
+      />
+      <Marker position={position}>
+        <Popup>サンプルの猫スポット</Popup>
+      </Marker>
+    </MapContainer>
+  );
+};
+
+export default Map;
